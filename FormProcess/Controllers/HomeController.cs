34b8c55097c69a5baf1c89ecebfd7f03206b1c8e
imranalways <57352521+imranalways@users.noreply.github.com>
2020-11-01 00:06:09 +0600

    Add files via upload

---
+++ FormProcess/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FormProcess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+
+namespace FormProcess.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person p)
+        {
+            TempData["person"] = p;
+            return RedirectToAction("showData");
+        }
+
+        public ActionResult showData()
+        {
+            Person p = TempData["person"] as Person;
+            return View(p);
+        }
+    }
+}
---
+++ FormProcess/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormProcess.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Dob { get; set; }
+        public string Bloodgroup { get; set; }
+        public string Gender { get; set; }
+    }
+}
